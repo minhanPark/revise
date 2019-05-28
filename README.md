b852f184aa9abf345eda0c31f59bfba8b197b03e
unknown <alsgks1218@naver.com>
2019-05-28 10:11:54 +0900

    initial setting

--- README.md
+++
@@ -1,2 +0,0 @@
-# revise
-strong revise
--- js/main.js
+++ js/main.js
@@ -1,86 +1,82 @@
-$(function () {
+$(function() {
+	$("header").each(function() {
+		var $window = $(window),
+			$header = $(this),
+			headerOffsetTop = $header.offset().top;
 
-    $('header').each(function () {
-        var $window = $(window),
-            $header = $(this),
+		$window.on("scroll", function() {
+			if ($window.scrollTop() > headerOffsetTop) {
+				$header.addClass("sticky");
+			} else {
+				$header.removeClass("sticky");
+			} //스크롤
+		});
 
-            headerOffsetTop = $header.offset().top;
+		$window.trigger("scroll");
+	});
 
+	$(".navi__item .ex").on("mouseover", function() {
+		$(".navi__item__down").css({ display: "block" });
+	});
 
-        $window.on('scroll', function () {
+	$(".navi__item .ex").on("mouseout", function() {
+		$(".navi__item__down").css({ display: "none" });
+	});
 
-            if ($window.scrollTop() > headerOffsetTop) {
-                $header.addClass('sticky');
-            } else {
-                $header.removeClass('sticky');
-            }; //스크롤
+	$(".navi__item__down").on("mouseover", function() {
+		$(".navi__item__down").css({ display: "block" });
+	});
 
-        });
+	$(".navi__item__down").on("mouseout", function() {
+		$(".navi__item__down").css({ display: "none" });
+	});
 
-            $window.trigger('scroll');
-    });
-
-    $('.navi__item .ex')
-      .on('mouseover', function(){
-        $('.navi__item__down').css({"opacity":"1"});
-
-    })
-
-    $('.navi__item .ex').on('mouseout', function(){
-      $('.navi__item__down').css({"opacity":"0"});
-    });
-
-    $('.navi__item__down')
-      .on('mouseover', function(){
-        $('.navi__item__down').css({"opacity":"1"});
-
-    })
-
-    $('.navi__item__down').on('mouseout', function(){
-      $('.navi__item__down').css({"opacity":"0"});
-    });
-
+	$(".hi").on("click", function() {
+		$(".top-wrapper__nav__down li").toggleClass("click");
+		$(".top-wrapper__nav__down li").slideToggle(300);
+	});
 });
 
-window.onload = function () {
-           var elm = ".box";
-           $(elm).each(function (index) {
-               // 개별적으로 Wheel 이벤트 적용
-               $(this).on("mousewheel DOMMouseScroll", function (e) {
-                   e.preventDefault();
-                   var delta = 0;
-                   if (!event) event = window.event;
-                   if (event.wheelDelta) {
-                       delta = event.wheelDelta / 120;
-                       if (window.opera) delta = -delta;
-                   }
-                   else if (event.detail)
-                       delta = -event.detail / 3;
-                   var moveTop = $(window).scrollTop();
-                   var elmSelecter = $(elm).eq(index);
-                   // 마우스휠을 위에서 아래로
-                   if (delta < 0) {
-                       if ($(elmSelecter).next() != undefined) {
-                           try{
-                               moveTop = $(elmSelecter).next().offset().top;
-                           }catch(e){}
-                       }
-                   // 마우스휠을 아래에서 위로
-                   } else {
-                       if ($(elmSelecter).prev() != undefined) {
-                           try{
-                               moveTop = $(elmSelecter).prev().offset().top;
-                           }catch(e){}
-                       }
-                   }
-
-                   // 화면 이동 0.8초(800)
-                   $("html,body").stop().animate({
-                       scrollTop: moveTop + 'px'
-                   }, {
-                       duration: 800, complete: function () {
-                       }
-                   });
-               });
-           });
-       }
+// 컨트롤 내릴때 한 영역씩
+// window.onload = function () {
+//            var elm = ".box";
+//            $(elm).each(function (index) {
+//                // 개별적으로 Wheel 이벤트 적용
+//                $(this).on("mousewheel DOMMouseScroll", function (e) {
+//                    e.preventDefault();
+//                    var delta = 0;
+//                    if (!event) event = window.event;
+//                    if (event.wheelDelta) {
+//                        delta = event.wheelDelta / 120;
+//                        if (window.opera) delta = -delta;
+//                    }
+//                    else if (event.detail)
+//                        delta = -event.detail / 3;
+//                    var moveTop = $(window).scrollTop();
+//                    var elmSelecter = $(elm).eq(index);
+//                    // 마우스휠을 위에서 아래로
+//                    if (delta < 0) {
+//                        if ($(elmSelecter).next() != undefined) {
+//                            try{
+//                                moveTop = $(elmSelecter).next().offset().top;
+//                            }catch(e){}
+//                        }
+//                    // 마우스휠을 아래에서 위로
+//                    } else {
+//                        if ($(elmSelecter).prev() != undefined) {
+//                            try{
+//                                moveTop = $(elmSelecter).prev().offset().top;
+//                            }catch(e){}
+//                        }
+//                    }
+//
+//                    // 화면 이동 0.6초(800)
+//                    $("html,body").stop().animate({
+//                        scrollTop: moveTop + 'px'
+//                    }, {
+//                        duration: 600, complete: function () {
+//                        }
+//                    });
+//                });
+//            });
+//        }
